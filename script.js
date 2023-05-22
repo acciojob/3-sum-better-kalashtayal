ea314db5ecd70647b93f32b7c0ad87bf95b29dd2
kalash tayal <130529290+kalashtayal@users.noreply.github.com>
2023-05-22 21:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function threeSum(arr, target) {
-// write your code here
+function threeSum(nums, target) {
+     // write your code here
+	// Sort the array in ascending order
+  nums.sort((a, b) => a - b);
+  
+  let closestSum = Infinity;
+  
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      
+      if (sum === target) {
+        // If the sum is equal to the target, return the sum
+        return sum;
+      }
+      
+      // Update the closest sum if the current sum is closer to the target
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  
+  return closestSum;
   
 }
 
